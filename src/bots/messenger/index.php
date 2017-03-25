8369e7916400639bb809d95bff413f95d22ff631
Fernando Lira <fhlira@gmail.com>
2017-03-25 15:08:41 -0300

    config

--- src/bots/messenger/index.php
+++ src/bots/messenger/index.php
@@ -2,9 +2,7 @@
 	/* 
 	https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received 
 	*/
-	define('BOT_TOKEN', "EAAERPIZCsMMwBANZCiFMYCwf4JiaPSFdDmzUSdTqv8hHLCd6Qo4wti1AbmbVODdI0fdNGr9emjfM5eNXmotj3d1m6EdwZBXV2EhUBimig2CVZCF8xPyBaOaOLFuSgEYx2NfbOKxqcZBSZC2ue0LzPuNwbQcAMj2Kk9E0Cbp4tsqgZDZD");
-	define('VERIFY_TOKEN', "curso_intellibots");
-	define('API_URL', 'https://graph.facebook.com/v2.6/me/messages?access_token='.BOT_TOKEN);
+	include 'config.php';
 	/*
 	Teste Tambem:
 	curl -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAAaki53jQwMBAGgy1sDUkjR0tQOyDFcVbKk1vavact1wiQ1dszOrAPiUuZCzdOL8bZCoVDdO9KpgqRyfVXSUEySUYwBgG6pijLRg0UC5AFG3MIxUMOAJarKPfvbBq58uIWXwdRZAxQeCACfhCCYbKQ2ccAvIPGVZAZAIXlcFJDAZDZD"
